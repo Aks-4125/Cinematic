1c38f8487c7b3fde7f27b67bfcd24097425ab6a0
Dhruvisha Darji <dhruvisha@promactinfo.com>
2018-05-22 14:36:31 +0530

    on bording feature

---
+++ app/src/main/java/com/training/cinematic/MyApplication.java
@@ -0,0 +1,33 @@
+package com.training.cinematic;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by dhruvisha on 5/18/2018.
+ */
+
+public class MyApplication extends Application {
+    public void OnCreate() {
+        super.onCreate();
+        Realm.init(getApplicationContext());
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name(Realm.DEFAULT_REALM_NAME)
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build()).build());
+
+    }
+}
