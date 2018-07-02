75e3a1077efd0153db8b570dddc0f4dc862c4834
dhruvisha <dhruvisha@promactinfo.com>
2018-07-02 17:10:03 +0530

    facebook login

--- app/src/main/java/com/training/cinematic/activity/Profile/ProfileActivity.java
+++ app/src/main/java/com/training/cinematic/activity/Profile/ProfileActivity.java
@@ -3,17 +3,26 @@ package com.training.cinematic.activity.Profile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.training.cinematic.Model.User;
 import com.training.cinematic.R;
 import com.training.cinematic.Utils.SharedPrefsHelp;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.Realm;
 
 public class ProfileActivity extends AppCompatActivity {
+    @BindView(R.id.image1)
+    ImageView profilePic;
+    @BindView(R.id.image)
+    ImageView proPic;
     @BindView(R.id.name)
     TextView fullname;
     @BindView(R.id.number)
@@ -23,9 +32,9 @@ public class ProfileActivity extends AppCompatActivity {
     @BindView(R.id.password)
     TextView password;
     Intent intent;
-    private static final String TAG =ProfileActivity.class.getName();
+    private static final String TAG = ProfileActivity.class.getName();
     String FLAG = "flag";
-
+    JSONObject response, profile_pic_data, profile_pic_url;
     String KEY_EMAIL = "email";
     private Realm realm;
 
@@ -36,11 +45,24 @@ public class ProfileActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         intent = getIntent();
         realm = Realm.getDefaultInstance();
-
-       email.setText(SharedPrefsHelp.getString(ProfileActivity.this,getString(R.string.get_email_pref),""));
+        String jsonData = SharedPrefsHelp.getString(ProfileActivity.this, "userProfile", null);
+        email.setText(SharedPrefsHelp.getString(ProfileActivity.this, getString(R.string.get_email_pref), ""));
         String stringEmail = email.getText().toString();
-
-
+        if (jsonData!=null) {
+            try {
+                response = new JSONObject(jsonData);
+                email.setText(response.get("email").toString());
+                fullname.setText(response.get("name").toString());
+                profile_pic_data = new JSONObject(response.get("picture").toString());
+                profile_pic_url = new JSONObject(profile_pic_data.getString("data"));
+                Picasso.with(this).load(profile_pic_url.getString("url"))
+                        .into(profilePic);
+                Picasso.with(this).load(profile_pic_url.getString("url"))
+                        .into(proPic);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         realm.beginTransaction();
         User user = realm.where(User.class)
                 .equalTo("emailId", stringEmail)
--- app/src/main/java/com/training/cinematic/activity/login/LoginController.java
+++ app/src/main/java/com/training/cinematic/activity/login/LoginController.java
@@ -1,5 +1,10 @@
 package com.training.cinematic.activity.login;
 
+import android.content.Context;
+
+import com.facebook.CallbackManager;
+import com.facebook.login.LoginResult;
+
 /**
  * Created by dhruvisha on 6/25/2018.
  */
@@ -8,6 +13,9 @@ public interface LoginController {
 
     interface ILoginView {
         boolean vaildate(String stringEmail, String stringPassword);
+
+        void loginCompleteWithFb(Context context);
+
     }
 
     interface ILoginPresenter {
@@ -16,6 +24,10 @@ public interface LoginController {
         boolean isPasswrodValid(String stringPassword);
 
         boolean checkUserNew(String stringEmail, String stringPassword);
+
+        void getUserDetails(LoginResult loginResult);
+
+        void loginWithFb(CallbackManager callbackManager, final Context context);
 
     }
 
